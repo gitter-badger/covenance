b1dd5ace404e29a02c7a0d747b7e2d3ac74ecae9
Victor Alvarez <v.alvarez312@gmail.com>
2015-08-21 20:51:17 -0700

    consume blueprint

--- src/blueprint.es6.js
+++ src/blueprint.es6.js
@@ -1,3 +1,36 @@
-let name = "victor";
+import mixin_a_lot from 'mixin-a-lot';
+import Scheme from '../scheme';
 
-console.log("hello ${name}");
+
+let BLUEPRINT_PROPERTY = 'blueprint';
+
+let blueprint =  mixin_a_lot.make_mixin({
+
+  name: 'blueprint',
+
+  premixing_hook: () => {
+    let blueprints = this[BLUEPRINT_PROPERTY];
+
+    if (!Array.isArray(blueprints)) {
+      throw new Error(`Expected property '${BLUEPRINT_PROPERTY}' to be an Array`)
+    } else {
+      for (let scheme of BLUEPRINT_PROPERTY) {
+        if (!(scheme instanceof Scheme)) {
+          throw new Error(
+            `Expected element ${scheme} of ${BLUEPRINT_PROPERTY} to be a Scheme`);
+        }
+      }
+    }
+  },
+
+  validate_class: () => {
+
+  },
+
+  validate_proto: () => {
+
+  }
+
+});
+
+
