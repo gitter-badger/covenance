5775f7bb607709c845a254a78433658b5a7dc715
Victor Alvarez <v.alvarez312@gmail.com>
2015-08-21 19:11:27 -0700

    babelify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,25 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import rename from 'gulp-rename';
 
 
 let [SRC, DST] = ['src', '.'];
 
 gulp.task('test', () => {
-  throw new Error("Write me!");
+  //throw new Error("Write me!");
+  console.log('Testing');
 });
 
 
 gulp.task('build', () => {
+  let clean = (path) => {
+    path.basename = path.basename.replace('.es6', '');
+    return path;
+  };
+
   return gulp.src([`${SRC}/**/*.es6.js`, `!${SRC}/**/*.spec.es6.js`])
     .pipe(babel())
+    .pipe(rename(clean))
     .pipe(gulp.dest(DST))
 });
 
