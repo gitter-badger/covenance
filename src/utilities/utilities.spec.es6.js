eef2ececb3da80ddf86568dd0a1d5d8e4705a31d
Victor Alvarez <v.alvarez312@gmail.com>
2015-08-22 16:30:34 -0700

    test "null" immutability using dirty bit

--- src/utilities/utilities.spec.es6.js
+++ src/utilities/utilities.spec.es6.js
@@ -3,27 +3,27 @@ import {immutable_descriptor_set} from './index.es6.js'
 
 let test_property = 'test_property';
 
-test('should create an immutable untyped property', t => {
-  let obj = Object.defineProperties({}, immutable_descriptor_set(test_property));
+test('should create the property', t => {
+  let obj = Object.defineProperties(
+    {}, immutable_descriptor_set(test_property));
 
   t.ok(obj.hasOwnProperty(test_property));
-  t.end();
+  t.end()
 });
 
-
-test('should allow setting property', t => {
-  t.plan(2);
-
-  let obj = Object.defineProperties({}, immutable_descriptor_set(test_property));
+test('should set the property', t => {
+  let obj = Object.defineProperties(
+    {}, immutable_descriptor_set(test_property));
 
   t.doesNotThrow(() => { obj[test_property] = true }, null);
   t.equals(obj[test_property], true);
+  t.end()
 });
 
-test('should not allow setting property twice', t => {
+test('should not set the property twice', t => {
   let obj = Object.defineProperties({}, immutable_descriptor_set(test_property));
 
-  obj[test_property] = true;
+  obj[test_property] = null;
 
   t.throws(() => {
     obj[test_property] = false
