cce571f29e71e5d3bb9c23f1534adfc1f5e60793
Victor Alvarez <v.alvarez312@gmail.com>
2015-08-21 23:04:10 -0700

    remove indirection

--- src/blueprint.es6.js
+++ src/blueprint.es6.js
@@ -23,10 +23,9 @@ let blueprint =  mixin_a_lot.make_mixin({
 
   validate_blueprint() {
     for (let scheme of this[BLUEPRINT_PROPERTY]) {
-      let [predicate, attribute] = [scheme.predicate, scheme.attribute];
-      let predicate_check = `${predicate}(${this[attribute]})`;
-      if (!eval(predicate_check)) {
-        throw new Error(`Expected '${predicate_check}' to return true`);
+      let check = `${scheme.predicate}(${this[scheme.attribute]})`;
+      if (!eval(check)) {
+        throw new Error(`Expected '${check}' to return true`);
       }
     }
   }
