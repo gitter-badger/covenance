f4b25d11d1d54ebaa37ae1c138e40d5d285a78d6
Victor Alvarez <v.alvarez312@gmail.com>
2015-08-23 19:18:08 -0700

    test blueprinting hooks

--- src/blueprint.spec.js
+++ src/blueprint.spec.js
@@ -3,13 +3,13 @@ import blueprint, {blueprinted, Scheme} from './blueprint'
 import {is_type} from './utilities'
 
 test('should enable blueprinting', t => {
-  t.notOk(Function.prototype.blueprint_proto);
-  t.notOk(Function.prototype.blueprint_static);
+  t.notOk(Function.prototype.proto_blueprint);
+  t.notOk(Function.prototype.static_blueprint);
 
   t.ok(blueprint.enable());
 
-  t.equals(typeof Function.prototype.blueprint_proto, 'function');
-  t.equals(typeof Function.prototype.blueprint_static, 'function');
+  t.equals(typeof Function.prototype.proto_blueprint, 'function');
+  t.equals(typeof Function.prototype.static_blueprint, 'function');
   t.end()
 });
 
@@ -22,10 +22,10 @@ test('should throw when blueprinting without "blueprint" property', t => {
   class Example {}
 
   t.throws(() => {
-    Example.blueprint_proto()
+    Example.proto_blueprint()
   }, /^TypeError: Expected property 'blueprint' to be an Array$/);
   t.throws(() => {
-    Example.blueprint_static()
+    Example.static_blueprint()
   }, /^TypeError: Expected property 'blueprint' to be an Array$/);
   t.end()
 });
@@ -37,10 +37,10 @@ test('should throw when blueprinting with wrongly typed "blueprint"', t => {
   E.blueprint = ['static_blueprint'];
 
   t.throws(() => {
-    E.blueprint_proto()
+    E.proto_blueprint()
   }, /^TypeError: Expected element 'proto_blueprint' of 'blueprint' to be a Scheme$/);
   t.throws(() => {
-    E.blueprint_static()
+    E.static_blueprint()
   }, /^TypeError: Expected element 'static_blueprint' of 'blueprint' to be a Scheme$/);
   t.end()
 });
@@ -57,7 +57,7 @@ test('should check proto blueprint', t => {
     Scheme({attribute: 'x', predicate: is_type('number')}),
     Scheme({attribute: 'y', predicate: is_type('number')})
   ];
-  Point.blueprint_proto();
+  Point.proto_blueprint();
 
   t.throws(() => {
     new Point(1, 'string')
@@ -70,7 +70,7 @@ test('should check static blueprint', t => {
   Example.blueprint = [
     Scheme({attribute: 'shortname', predicate: is_type('string')})
   ];
-  Example.blueprint_static();
+  Example.static_blueprint();
 
   t.throws(() => {
     Example.shortname = true;
@@ -83,13 +83,41 @@ test('should check static blueprint', t => {
   t.end()
 });
 
-test.skip('should support before_blueprint option', t => {
+test('should support a before_blueprint hook on static blueprint', t => {
+  class Example {}
+  Example.blueprint = [
+    Scheme({attribute: 'shortname', predicate: is_type('string')})
+  ];
+
+  t.doesNotThrow(() => {
+    Example.static_blueprint({
+      before_blueprint() {
+        this.shortname = 'example'
+      }
+    });
+  });
+  t.equals(Example.shortname, 'example');
+  t.end()
+});
+
+test('should support a before_blueprint hook on proto blueprint', t => {
+  class Example {}
+  Example.prototype.blueprint = [
+    Scheme({attribute: 'shortname', predicate: is_type('string')})
+  ];
+
+  t.doesNotThrow(() => {
+    Example.proto_blueprint({
+      before_blueprint() {
+        this.shortname = 'example'
+      }
+    });
+  });
+  t.equals(Example.prototype.shortname, 'example');
+  t.end()
 });
 
 test.skip('should support before_check_hook option', t => {
-});
-
-test.skip('should support after_blueprint option', t => {
 });
 
 test.skip('should support after_check_hook option', t => {
