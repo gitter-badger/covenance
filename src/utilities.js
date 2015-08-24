ba7a1bbfdd92c4dd2587e519b98385a9b3d933c4
Victor Alvarez <v.alvarez312@gmail.com>
2015-08-23 20:12:28 -0700

    refactor frosty out

--- src/utilities.js
+++ src/utilities.js
@@ -13,32 +13,5 @@ const is_type = (type) => {
   }
 };
 
-const immutable_descriptor_set = property => {
-  let privatized = Symbol(property);
-  let UNINITIALIZED = undefined;
 
-  return {
-    [privatized]: {
-      value: UNINITIALIZED,
-      writable: true
-    },
-
-    [property]: {
-      enumerable: true,
-      get() {
-        return this[privatized];
-      },
-      set(value) {
-        if (this[privatized] !== UNINITIALIZED) {
-          throw new Error(`${property} is immutable`)
-        } else if (value === UNINITIALIZED) {
-          throw new Error(`${property} should be defined`)
-        }
-        this[privatized] = value
-      }
-    }
-  }
-};
-
-
-export default {merge, is_type, immutable_descriptor_set}
+export default {merge, is_type}
