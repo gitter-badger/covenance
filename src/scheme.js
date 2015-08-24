164125d299c46d85ded5d0134c09c118a4fad000
Victor Alvarez <v.alvarez312@gmail.com>
2015-08-23 21:20:28 -0700

    Expand Scheme creation attributes

--- src/scheme.js
+++ src/scheme.js
@@ -7,23 +7,41 @@ class Scheme {
     this.attribute = attribute;
     this.predicate = predicate;
   }
-
-  static value_of({attribute, predicate} = {}) {
-    if (attribute === undefined || predicate === undefined) {
-      throw new Error('Expected {attribute: [string], predicate: [function]}')
-    } else if (typeof attribute !== 'string') {
-      throw new Error(`Expected ${attribute} to be a string`)
-    } else if (typeof predicate !== 'function') {
-      throw new Error(`Expected ${predicate} to be a function`)
-    }
-    return new this({attribute, predicate})
-  }
 }
 
 frosty.freeze(Scheme.prototype, 'attribute', 'predicate');
 
+const USAGE = `
+Expected {attribute: [string], predicate: [function]}, or ([string], [function]).`;
+
+let check_args = (attribute, predicate) => {
+  if (typeof attribute !== 'string') {
+    throw new Error(`Expected ${attribute} to be a string`)
+  } else if (typeof predicate !== 'function') {
+    throw new Error(`Expected ${predicate} to be a function`)
+  }
+};
+
 export default {
-  Scheme: Scheme.value_of.bind(Scheme),
+  Scheme(...args) {
+    let attribute, predicate;
+    if (args.length === 2) {
+      attribute = args[0];
+      predicate = args[1];
+    } else if (args.length === 1) {
+      let named = args[0];
+      if (typeof named !== 'object') {
+        throw new Error(USAGE)
+      } else {
+        attribute = named.attribute;
+        predicate = named.predicate;
+      }
+    } else {
+      throw new Error(USAGE)
+    }
+    check_args(attribute, predicate);
+    return new Scheme({attribute, predicate})
+  },
 
   is_Scheme(thing) {
     return thing instanceof Scheme
--- src/scheme.spec.js
+++ src/scheme.spec.js
@@ -3,12 +3,24 @@ import {Scheme} from './scheme'
 import {is_type} from './utilities'
 
 
-
-
 test('should throw when creating with wrong signature', t => {
   t.throws(() => {
     Scheme()
   }, /Expected \{attribute: \[string], predicate: \[function]}/);
+  t.throws(() => {
+    Scheme(1, 2, 3)
+  }, /Expected \{attribute: \[string], predicate: \[function]}, or \(\[string], \[function]\)\./);
+  t.end()
+});
+
+
+test('should not throw when creating with right signature', t => {
+  t.doesNotThrow(() => {
+    Scheme({attribute: 'string', predicate: () => {}})
+  }, /Expected \{attribute: \[string], predicate: \[function]}/);
+  t.doesNotThrow(() => {
+    Scheme('string', () => {})
+  }, /Expected \{attribute: \[string], predicate: \[function]}, or \(\[string], \[function]\)\./);
   t.end()
 });
 
@@ -18,6 +30,12 @@ test('should throw when creating with wrong types', t => {
   }, new RegExp(`Expected ${1} to be a string`));
   t.throws(() => {
     Scheme({attribute: 'string', predicate: []})
+  }, new RegExp(`Expected ${[]} to be a function`));
+  t.throws(() => {
+    Scheme(1, () => {})
+  }, new RegExp(`Expected ${1} to be a string`));
+  t.throws(() => {
+    Scheme('string', [])
   }, new RegExp(`Expected ${[]} to be a function`));
   t.end();
 });
