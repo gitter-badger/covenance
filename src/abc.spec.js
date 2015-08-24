daba32680af5deb5d4857786a07410a752618c2c
Victor Alvarez <v.alvarez312@gmail.com>
2015-08-24 16:23:15 -0700

    Empty unit tests for ABC

---
+++ src/abc.spec.js
@@ -0,0 +1,22 @@
+import test from 'tape'
+
+
+test('should thrown an error when instantiated directly', t => {
+
+});
+
+test('should throw an error when implementation has no blueprint', t => {
+
+});
+
+test('should throw an error when implementation does not satisfy blueprint', t => {
+
+});
+
+test('should expose abstract methods to implementations', t => {
+
+});
+
+test('should allow implementations to override abstract methods', t => {
+
+});
