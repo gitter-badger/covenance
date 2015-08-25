2219ff82f8478543c6d62ab887f07cc6a016921b
Victor Alvarez <v.alvarez312@gmail.com>
2015-08-24 21:44:25 -0700

    rename: acts_as_blueprint, BLUEPRINT_KEY

--- src/mixin.js
+++ src/mixin.js
@@ -2,27 +2,27 @@ import mixin_a_lot from 'mixin-a-lot';
 import {Blueprint, is_Blueprint} from './blueprint';
 
 
-const BLUEPRINT_NAME = 'blueprint';
+const BLUEPRINT_KEY = 'blueprint';
 const acts_as_blueprint = mixin_a_lot.make_mixin({
 
-  name: BLUEPRINT_NAME,
+  name: 'acts_as_blueprint',
 
   premix() {
-    let blueprints = this[BLUEPRINT_NAME];
+    let blueprints = this[BLUEPRINT_KEY];
 
     if (!Array.isArray(blueprints)) {
-      throw new TypeError(`Expected property '${BLUEPRINT_NAME}' to be an Array`)
+      throw new TypeError(`Expected property '${BLUEPRINT_KEY}' to be an Array`)
     }
     for (let scheme of blueprints) {
       if (!(is_Blueprint(scheme))) {
         throw new TypeError(
-          `Expected element '${scheme}' of '${BLUEPRINT_NAME}' to be a Blueprint`);
+          `Expected element '${scheme}' of '${BLUEPRINT_KEY}' to be a Blueprint`);
       }
     }
   },
 
   check_blueprint() {
-    for (let scheme of this[BLUEPRINT_NAME]) {
+    for (let scheme of this[BLUEPRINT_KEY]) {
       if (!scheme.predicate(this[scheme.attribute])) {
         throw new TypeError(
           `'${scheme.attribute}': '${this[scheme.attribute]}' failed blueprint check`);
