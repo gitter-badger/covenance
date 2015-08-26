49e6d91c562dbd4ee011c07dae92bc85c17b90ec
Victor Alvarez <v.alvarez312@gmail.com>
2015-08-25 20:27:11 -0700

    +refactor and expose check_blueprint

--- src/mixin.js
+++ src/mixin.js
@@ -2,6 +2,15 @@ import mixin_a_lot from 'mixin-a-lot';
 import {Blueprint, is_Blueprint} from './blueprint';
 
 
+let check_blueprint = (context, blueprint) => {
+  for (let scheme of blueprint) {
+    if (!scheme.predicate(context[scheme.attribute])) {
+      throw new TypeError(
+        `'${scheme.attribute}': '${context[scheme.attribute]}' failed blueprint check`);
+    }
+  }
+};
+
 const BLUEPRINT_KEY = 'blueprint';
 const acts_as_blueprint = mixin_a_lot.make_mixin({
 
@@ -22,13 +31,9 @@ const acts_as_blueprint = mixin_a_lot.make_mixin({
   },
 
   check_blueprint() {
-    for (let scheme of this[BLUEPRINT_KEY]) {
-      if (!scheme.predicate(this[scheme.attribute])) {
-        throw new TypeError(
-          `'${scheme.attribute}': '${this[scheme.attribute]}' failed blueprint check`);
-      }
-    }
+    check_blueprint(this, this[BLUEPRINT_KEY])
   }
+
 });
 
-export default {acts_as_blueprint}
+export default {acts_as_blueprint, check_blueprint}
