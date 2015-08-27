29b3939125cee908ce0b1e685ba0cd07ea115cfd
Victor Alvarez <v.alvarez312@gmail.com>
2015-08-26 19:02:38 -0700

    remove needless method now

--- src/utilities.js
+++ src/utilities.js
@@ -13,20 +13,4 @@ const is_type = (type) => {
   }
 };
 
-const inherit = (child, parent) => {
-  for (let key in parent) {
-    if (parent.hasOwnProperty(key)) {
-      child[key] = parent[key];
-    }
-  }
-  let ctor = () => {
-    this.constructor = child;
-  };
-  ctor.prototype = parent.prototype;
-  child.prototype = new ctor();
-  child.__super__ = parent.prototype;
-  return child;
-};
-
-
-export default {merge, is_type, inherit}
+export default {merge, is_type}
