d25a7b4e32bd4827d1cafd5fb676916626688809
Victor Alvarez <v.alvarez312@gmail.com>
2015-08-29 11:36:15 -0700

    Adding assert_some_fn utility

--- src/utilities.js
+++ src/utilities.js
@@ -1,10 +1,15 @@
-const merge_own = (obj1, obj2) => {
-  for (let key in obj2) {
-    if (obj2.hasOwnProperty(key)) {
-      obj1[key] = obj2[key]
+const merge_own = (target, ...sources) => {
+  let merge_one = (obj) => {
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        target[key] = obj[key]
+      }
     }
+  };
+  for (let source of sources) {
+    merge_one(source)
   }
-  return obj1
+  return target
 };
 
 const is_type_of = (type) => {
@@ -13,12 +18,38 @@ const is_type_of = (type) => {
   }
 };
 
+let is_string = is_type_of('string');
+let is_function = is_type_of('function');
+let is_number = is_type_of('number');
+let is_object_literal = (thing) => {
+  let is_object = is_type_of('object');
+  return is_object(thing) && !Array.isArray(thing)
+};
+
+const assert_some_fn = (message = '', ...fns) => {
+  let ok_fn = (fn) => {
+    if (!is_function(fn)) {
+      throw new Error(`Expected function for ${fn}`)
+    }
+  };
+  let fails = 0;
+  for (let fn of fns) {
+    try {
+      ok_fn(fn) && fn()
+    } catch (e) {
+      fails++;
+    }
+  }
+  if (fails === fns.length) {
+    throw new Error(message || `All ${fns.length} functions failed`)
+  }
+};
+
 export default {
   merge_own,
-  is_string: is_type_of('string'),
-  is_function: is_type_of('function'),
-  is_number: is_type_of('number'),
-  is_object_literal(thing) {
-    return typeof thing === 'object' && !Array.isArray(thing)
-  }
+  assert_some_fn,
+  is_string,
+  is_function,
+  is_number,
+  is_object_literal
 }
